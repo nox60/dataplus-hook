e40213e207d023c2716322c90d7faabe398e3bdc
LiuLi <nox60@hotmail.com>
2021-11-25 18:14:09 +0800

    Update hook implementation

--- src/main/java/com/dataxplus/hook/utils/HttpReq.java
+++ src/main/java/com/dataxplus/hook/utils/HttpReq.java
@@ -1,16 +1,23 @@
 package com.dataxplus.hook.utils;
 
 import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HttpReq {
 
@@ -23,9 +30,19 @@ public class HttpReq {
     public static String postMsg(String sendurl, String data) {
         CloseableHttpClient client = HttpClients.createDefault();
         HttpPost post = new HttpPost(sendurl);
-        StringEntity myEntity = new StringEntity(data,
-                ContentType.APPLICATION_JSON);// 构造请求数据
-        post.setEntity(myEntity);// 设置请求体
+//        StringEntity myEntity = new StringEntity(data,
+//                ContentType.APPLICATION_JSON);// 构造请求数据
+        List<NameValuePair> nameValuePairList = new ArrayList<NameValuePair>();
+        nameValuePairList.add(new BasicNameValuePair("jboResult", data));
+        StringEntity reqEntity = null;
+        try {
+            reqEntity = new UrlEncodedFormEntity(nameValuePairList, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        post.setEntity(reqEntity);// 设置请求体
+        post.setHeader(new BasicHeader("Content-Type", "application/x-www-form-urlencoded; charset=utf-8"));
+        post.setHeader(new BasicHeader("Accept", "text/plain;charset=utf-8"));
         String responseContent = null; // 响应内容
         CloseableHttpResponse response = null;
         try {
